8a1f946c2caf3dae1ff6fbd033a39a1026d7834a
Nirgal <znirgal@gmail.com>
2017-10-18 17:59:24 +0200

    ble

--- tp.sql
+++ tp.sql
@@ -1,11 +1,21 @@
 #EX01 ====================================================================================================
 #1) Créer une base de données nommée 'entreprise'.
 
+CREATE DATABASE entreprise;
+
 #2) Mettre la base entreprise par défaut pour la session
+
+USE entreprise;
 
 #3) Voir quel encodage et collation sont utilisés dans cette base
 
+SELECT @@character_set_database, @@collation_database;
+
 #4) Changer l'encodage et la collation pour qu'ils se basent sur UTF8
+
+ALTER DATABASE entreprise CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci';
+
+
 
 #=========================================================================================================
 
@@ -20,6 +30,17 @@
 #un salaire 'salaire' permettant de stocker des salaires jusqu'à 1234567,89 €
 #un champ 'test' permettant de stocker 120 caractères.
 
+CREATE TABLE employes (
+	id INT NOT NULL AUTO_INCREMENT,
+    dateEmbauche DATE NOT NULL,
+    matricule VARCHAR(6),
+    nom VARCHAR(50),
+    prenom VARCHAR(10),
+    salaire DECIMAL(9,2),
+    test VARCHAR(120),
+    PRIMARY KEY (id)
+);
+
 
 #=========================================================================================================
 
@@ -28,13 +49,28 @@
 #Modifier la table Employe : 
 #1) Supprimer le champ 'test'
 
+ALTER TABLE employes
+DROP column employes.test;
+
 #2) Ajouter une colonne 'tempsPartiel' de type booléen avec comme valeur par défaut false, après la date d'embauche
+
+ALTER TABLE employes
+ADD COLUMN tempsPartiel boolean DEFAULT false AFTER dateEmbauche;
 
 #3) Ajouter une colonne 'sexe' permettant de stocker 'M'ou 'F' avec comme valeur par défaut 'M', après le prénom
 
+ALTER TABLE employes
+ADD COLUMN sexe VARCHAR(1) DEFAULT 'M' CHECK (sexe = 'M' || sexe = 'F') AFTER prenom ;
+
 #4) Modifier la colonne 'prenom' pour pouvoir stocker 50 caractères et refuser une valeur nulle
 
+ALTER TABLE employes
+MODIFY COLUMN prenom VARCHAR(50) NOT NULL;
+
 #5) Modifier la colonne 'nom' pour refuser une valeur nulle
+
+ALTER TABLE employes
+MODIFY COLUMN nom VARCHAR(50) NOT NULL;
 
 #=========================================================================================================
 
@@ -42,15 +78,28 @@
 #EX04 ====================================================================================================
 #1) Ajouter une contrainte d'unicité nommée matricule_unique sur la colonne matricule
 
+ALTER TABLE employes
+ADD CONSTRAINT matricule_unique UNIQUE (matricule);
 #=========================================================================================================
 
 
 #EX05 ====================================================================================================
 #1) Insérer Vincent Scheider, matricule C11106, embauché le 1er janvier 2001 à 1180,27 €
 
+
+INSERT INTO employes (prenom,nom,sexe,matricule,dateEmbauche,salaire)
+VALUES ('Vincent', 'Scheinder', 'M', 'C11106', '2001-01-01', 1180.27);
+
 #2) Insérer Victor Gaillard, matricule M11109, embauché à temps partiel le 4 avril 2004 à 1480,30 €
 
+INSERT INTO employes (prenom,nom,sexe,tempsPartiel, matricule,dateEmbauche,salaire)
+VALUES ('Victor', 'Gaillard', 'M', true, 'M11109', '2004-04-04', 1480.30);
+
 #3) Insérer Clémence Roussel, matricule T11101, embauchée le 4 avril 2004 à 1680,32 €
+
+INSERT INTO employes (prenom,nom, sexe, matricule,dateEmbauche,salaire)
+VALUES ('Clémence', 'Roussel', 'F', 'T11101', '2004-04-04', 1680.32);
+
 
 #=========================================================================================================
 
@@ -60,17 +109,41 @@
 #mysql -u root -proot -D entreprise < employes.sql
 #2) Récupérer tous les employés
 
+SELECT * FROM employes;
+
 #3) Compter le nombre d'employés (2503)
+
+SELECT count(*) FROM employes;
 
 #4) Récupérer les employés qui gagnent plus de 1500 € (1665)
 
+SELECT count(*) 
+FROM employes
+WHERE salaire > 1500;
+
 #5) Récupérer les employés dans l'ordre décroissant de leur matricule (Clémence Roussel en premier)
+
+SELECT * 
+FROM employes
+ORDER BY matricule DESC;
 
 #6) Récupérer toutes les femmes de l'entreprise (1254)
 
+SELECT *
+FROM employes
+WHERE sexe = 'F';
+
 #7) Récupérer tous les employés à temps partiel embauchés après le 1er janvier 2007 (1244)
 
+SELECT count(*)
+FROM employes
+WHERE tempsPartiel = 1 AND dateEmbauche > '2007-01-01';
+
 #8) En une seule requête, compter le nombre de femme et d'homme de l'entreprise. (M : 1249, F : 1254)
+
+SELECT count(*)
+FROM employes
+GROUP BY sexe;
 
 #9) Récupérer le nombre d'embauche par année : 
 #2001:1
@@ -83,20 +156,39 @@
 #2014:408
 #2015:385
 
+SELECT count(year(dateEmbauche))
+FROM employes
+GROUP BY year(dateEmbauche);
+
 #10) Récupérer le nombre de techniciens (matricule qui commence par T), le nombre de commerciaux (matricule commencant par C) et le nombre de managers (matricule commençant par M)
 #C:432
 #M:420
 #T:1651
 
+SELECT count(*)
+FROM employes
+GROUP BY substr(matricule,1,1);
+
+
 #11) Récupérer le nombre de personnes à temps partiel et à temps plein
 #0:1259
 #1:1244
 
+SELECT count(*)
+FROM employes
+GROUP BY tempsPartiel;
+
 #12) Récupérer tous les employés dont le prénom contient un M (majuscule ou minuscule) (738)
+
+SELECT count(*) 
+FROM employes
+WHERE prenom LIKE '%m%';
 
 #13) Récupérer le salaire minimum, maximum et moyen de tous les employés
 #1000.00, 2500.00, 1749.790208
 
+SELECT min(salaire), max(salaire), avg(salaire)
+FROM employes;
 #=========================================================================================================
 
 
@@ -104,13 +196,39 @@
 #1) Faire passer le technicien de matricule T00027 à mi-temps (et donc diviser son salaire par 2...)
 #Temps partiel : TRUE, salaire : 511.50
 
+UPDATE employes
+set tempsPartiel = TRUE, salaire = 511.50
+WHERE matricule = 'T00027';
+
+SELECT tempsPartiel, salaire
+FROM employes
+WHERE matricule = 'T00027';
+
 #=========================================================================================================
 
 
 #EX08 ====================================================================================================
 #1) Licencier l'employé T00115
 
+DELETE FROM employes
+WHERE matricule = 'T00115';
+
 #2) Licencier les 5 commerciaux qui gagnent le plus d'argent
+
+DELETE FROM employes
+WHERE matricule IN (
+SELECT matricule
+FROM employes
+WHERE matricule LIKE 'C%'
+ORDER BY salaire DESC
+LIMIT 5);
+
+
+SELECT matricule
+FROM employes
+WHERE matricule LIKE 'C%'
+ORDER BY salaire DESC
+LIMIT 5;
 
 #ID: 1540, 1211, 2359, 1668, 1748
 #=========================================================================================================
